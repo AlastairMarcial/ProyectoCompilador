02c64694450c8cb64ac0e77f3d2000eb6dbb9ebe
AlastairMarcial <63171626+AlastairMarcial@users.noreply.github.com>
2020-04-04 19:16:00 -0600

    Hace unos dias hicimos esto :( Es humilde pero trabajo honrado

--- ProyectoCompilador/ProyectoCompilador/Recursos/Gramatica.cs
+++ ProyectoCompilador/ProyectoCompilador/Recursos/Gramatica.cs
@@ -4,11 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Irony.Ast;
-using Irony.Ast;
+using Irony.Parsing;
 
 namespace ProyectoCompilador.Recursos
 {
-    class Gramatica
+    class Gramatica:Grammar
     {
+        public Gramatica():base(caseSensitive:true) 
+        {
+
+            #region er
+            StringLiteral CADENA = new StringLiteral("cadena", "\"");
+            var ENTERO = new NumberLiteral("entero");
+            var DECIMAL = new RegexBasedTerminal("Decimal", "[0-9]+'.'[0-9]+");
+            IdentifierTerminal IDENTIFICADOR = new IdentifierTerminal("ID");
+
+            CommentTerminal comentarioBloque = new CommentTerminal("comentarioBloque", "/", "/");
+            #endregion
+
+            #region Terminales
+
+            #endregion
+
+            #region No terminales
+            #endregion
+
+            #region Gramatica
+            #endregion
+
+            #region Preferencias
+            #endregion
+        }
     }
 }
